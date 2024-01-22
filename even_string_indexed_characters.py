1f5eead453c156af01af2ce487a0186e476d6c18
MJ Gillado <mjgillado0@gmail.com>
2024-01-22 23:12:57 +0800

    Created pseudocode

--- even_string_indexed_characters.py
+++ even_string_indexed_characters.py
@@ -1,2 +1,13 @@
-# Write a program to accept a string from the user and display characters that are present at an even index number.
+# Write a program to accept a string from the user and 
+#display characters that are present at an even index number.
 
+# Pseudocode
+
+# User's input name
+
+#Print name
+
+# Create a loop for even indices
+
+    # Print name and index values
+    
